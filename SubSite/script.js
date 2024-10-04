88eaab0cf91f6956d912c9182a020a2f0b5a8da8
Darko <48098927+Darko1green1LOrd@users.noreply.github.com>
2024-10-04 21:18:35 +0000

    Small fixes, added settings and made main page accesible from settings

--- SubSite/script.js
+++ SubSite/script.js
@@ -6,17 +6,87 @@ function disableSpellcheck(){
     }
 }
 
+function runonload(){
+    disableSpellcheck();
+    run_customselect();
+    changetheme();
+    ge("main_p","geba").style.display = "inline-block";
+}
+
+function changetheme(inp=null,elem=null){
+    if(inp == null){inp = ge("stylesel-db").value;}
+    let root = document.documentElement;
+    ge("stylesel_div").style.display = (inp == "c") ? "block" : "";
+    ge("stylesel_emergency_toggle","geba").style.display = (inp == "c") ? "block" : "";
+    ge("emergency_sr").style.display = (inp == "c" && ge("stylesel_t").checked) ? "block" : "";
+
+    if (inp == "reset"){
+        ge("stylesel-db").selectedIndex = 1
+        const selected_itm = ge("stylesel-db");
+        ge("select-selected","geba").innerHTML = selected_itm.options[selected_itm.selectedIndex].innerHTML;
+    }
+
+    var styles = {
+        "dg":{
+            "--bodycolor": "#000",
+            "--inputcolor": "#171817",
+            "--contpanel-colone": "none",
+            "--contpanel-coltwo": "none"
+        }
+    }
+
+    var all_stylevars = ge("stylesel_table").children[0].children;
+    var all_stylevars_length = all_stylevars.length;
+    for (var asv = 1; asv < all_stylevars_length; asv++) {
+        elem = all_stylevars[asv].children[1].children[0];
+        if (inp != "c"){elem.value = elem.dataset.defvalue;}
+        if (Object.keys(styles).indexOf(inp) != -1){ //Check if the selected input is in styles
+            if (Object.keys(styles[inp]).indexOf(elem.dataset.varid) != -1){
+                elem.value = styles[inp][elem.dataset.varid];
+            }
+        }
+        var colortile = elem.parentElement.parentElement.children[2];
+        colortile.style.backgroundColor = "";colortile.style.backgroundColor = (elem.value != "") ? elem.value : elem.dataset.defvalue;
+        root.style.setProperty(elem.dataset.varid, elem.value);
+    }
+}
+
+function settingChanged(elem){
+    const elem_id = elem.id;
+    if (elem_id == "msgdura_v"){
+        if (elem.value == ""){
+            elem.value = (elem.oldvalue >= 0 && elem.oldvalue <= 100) ? elem.oldvalue : 6;
+            showmsg("Error","This field cannot be empty","Restoring previous value");
+        }
+    }
+    else if (elem_id == "stylesel-db"){changetheme(elem.value);}
+    else if (elem.classList[0] == "customstyle"){
+        let root = document.documentElement;
+        var colortile = elem.parentElement.parentElement.children[2];
+        colortile.style.backgroundColor = "";colortile.style.backgroundColor = (elem.value != "") ? elem.value : elem.dataset.defvalue;
+        root.style.setProperty(elem.dataset.varid, elem.value);
+    }
+    else if (elem_id == "stylesel_t"){ge("emergency_sr").style.display = (ge("stylesel-db").value == "c" && ge("stylesel_t").checked) ? "block" : "";}
+
+}
+
+function ge(elem,mode="gebi"){
+    if (mode == "qs"){return document.querySelector("#"+elem);}
+    else if (mode == "gebi"){return document.getElementById(elem);}
+    else if (mode == "geba"){return document.getElementsByClassName(elem)[0];}
+}
+
 function hidemsg(){
-    const trigger = document.querySelector('#infobox-t');
+    const trigger = ge('infobox-t');
     trigger.checked = false;
 }
 
-function showmsg(titlevar,text1var,text2var="",duration=6000){
-    const trigger = document.querySelector('#infobox-t');
-    const title = document.querySelector('#infobox-title');
-    const text_1 = document.querySelector('#infobox-first');
-    const text_2 = document.querySelector('#infobox-second');
-    const ib_button = document.querySelector('#infobox-btn');
+function showmsg(titlevar,text1var,text2var="",duration=Number(ge("msgdura_v").value)*1000){
+    const trigger = ge('infobox-t');
+    const title = ge('infobox-title');
+    const text_1 = ge('infobox-first');
+    const text_2 = ge('infobox-second');
+    const ib_button = ge('infobox-btn');
 
     title.innerHTML = titlevar;
     text_1.innerHTML = text1var;
@@ -30,8 +100,16 @@ function showmsg(titlevar,text1var,text2var="",duration=6000){
     }
 }
 
-function runonload(){
-    disableSpellcheck();
+function switch_mode(ind){
+    const mainbtn = ge('main_mode');
+    const settingsmbtn = ge('settings_mode');
+    const main = ge("main_p","geba");
+    const settingsm = ge("settingsp","geba");
+
+    mainbtn.classList = (ind == 0) ? "selected" : "";
+    settingsmbtn.classList = (ind == 1) ? "selected" : "";
+    main.style.display = (ind == 0) ? "inline-block" : "";
+    settingsm.style.display = (ind == 1) ? "inline-block" : "";
 }
 
 function vist_page(link){
